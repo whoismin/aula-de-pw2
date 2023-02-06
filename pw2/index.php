b8b7a13ecb09d7daca661db503dd524622402e91
whoismin <99838930+whoismin@users.noreply.github.com>
2023-02-06 16:31:15 -0300

    Add files via upload

---
+++ pw2/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$a = 1;
+$b = 2;
+
+echo "adição: " . $a + $b . "<br>";
+echo "subtração: " . $a - $b . "<br>";
+echo "multi: " . $a * $b . "<br>";
+echo "divis: " . $a / $b . "<br>";
+
+?>
